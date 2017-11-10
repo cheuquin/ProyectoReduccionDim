8fb7db2af3efe1c8529309294ec22956ecc667d7
IMartinezA <33355502+IMartinezA@users.noreply.github.com>
2017-11-10 11:36:26 -0300

    Última versión de la matriz de signo
    El código está completo, solo falta arreglar los errores, está marcado dónde es que no sé que pasa. por el momento la matriz que se obtiene es densa y llena de 1, porque aun no le agrego el Delta de ustedes (por eso es densa) y no he arreglado el error (por eso tiene puros 1).

--- DesAleatorizado.py
+++ DesAleatorizado.py
@@ -37,12 +37,12 @@ PesimistaMas=np.ones((n))
 PesimistaMenos=np.ones((n))
 
 #L\'inea 3 del algoritmo 2: La lista vacia de los valores sigma.
-sigma=[]
+sigma=sp.lil_matrix((m,d),dtype='float')
 
-ThetaMas=sp.lil_matrix((n,m))
-ThetaMenos=sp.lil_matrix((n,m))
+ThetaMas=sp.lil_matrix((n,m),dtype='float')
+ThetaMenos=sp.lil_matrix((n,m),dtype='float')
 
-nu=sp.lil_matrix((n,m))
+nu=sp.lil_matrix((n,m)dtype='float')
 
 
 #Comienzan los c\'alculos de PesimistaMas
@@ -60,23 +60,43 @@ for r in range(0,m):  #L\'inea 5 del algoritmo 2
                 nu[i,r]=0.
             if b[i]==1.:
                 temp=(V.getcol(j).getrow(i).toarray()[0][0])**2
-                ThetaMas[i,r]=ThetaMas[i,r]-temp/(1+temp)
-                ThetaMenos[i,r]=ThetaMenos[i,r]+temp/(1-temp)
-                PesimistaMas[i]=PesimistaMas[i]*np.sqrt(1+temp/2)**(-1)
-                PesimistaMenos[i]=PesimistaMenos[i]*np.sqrt(1-temp/2)**(-1)
+                ThetaMas[i,r]=ThetaMas[i,r]-temp/(1.+temp)
+                ThetaMenos[i,r]=ThetaMenos[i,r]+temp/(1.-temp)
+                PesimistaMas[i]=PesimistaMas[i]*np.sqrt(1.+temp/2.)**(-1)
+                PesimistaMenos[i]=PesimistaMenos[i]*np.sqrt(1.-temp/2.)**(-1)
         for i in range(0,n):
             if b[i]==1.:
                 PesimistaMas[i]=PesimistaMas[i]*np.sqrt(ThetaMas[i,r])**(-1)
                 PesimistaMenos[i]=PesimistaMenos[i]*np.sqrt(ThetaMenos[i,r])**(-1)
         b=np.zeros((n))
-            
+#Fase 2
+for r in range(0,m):
+    NZIndicesDeltar=Delta.getrow(r).nonzero()[1]
+    for j in NZIndicesDeltar:
+        NZIndicesVj=V.getcol(j).nonzero()[0]
+        print NZIndicesVj
+        for i in NZIndicesVj:
+            temp=(V.getcol(j).getrow(i).toarray()[0][0])**2
+            PesimistaMas[i]=PesimistaMas[i]*np.sqrt(ThetaMas[i,r])*np.sqrt(1.+temp/2.)*np.exp(-(nu[i,r]**2*(s**2*(1.-ThetaMas[i,r])/ThetaMas[i,r]+1.)+temp)/4.)
+            PesimistaMenos[i]=PesimistaMenos[i]*np.sqrt(ThetaMenos[i,r])*np.sqrt(1.-temp/2.)*np.exp((nu[i,r]**2*(s**2*(1.-ThetaMenos[i,r])/ThetaMenos[i,r]+1.)+temp)/4.)
+            ThetaMas[i,r]=ThetaMas[i,r]+temp/(1.+temp/2.)
+            ThetaMenos[i,r]=ThetaMenos[i,r]+temp/(1.+temp/2.)
+        ExpresionParaSigmaMenos1=0.
         
-
-
-
-
-
-#Lo que sigue despu\'es del inicio
+        ExpresionParaSigmaMas1=0.
+        #Falta el armado de las expresiones (linea 6 del c\'odigo)
+        if ExpresionParaSigmaMenos1>ExpresionParaSigmaMas1:
+            sigma[r,j]=1.
+        else:
+            sigma[r,j]=-1
+        #Duda en la siguiente seccion, hay ambiguedad con los indices
+        for i in NZIndicesVj:
+            temp=V.getcol(j).getrow(i).toarray()[0][0]
+            nu[i,r]=nu[i,r]+sigma[r,j]*temp
+            PesimistaMas[i]=PesimistaMas[i]*np.sqrt(ThetaMas[i,r])**(-1)*np.exp(nu[i,r]**2*(s**2*(1-ThetaMas[i,r])/ThetaMas[i,r])/4.)
+            PesimistaMenos[i]=PesimistaMenos[i]*np.sqrt(ThetaMenos[i,r])**(-1)*np.exp(-nu[i,r]**2*(s**2*(1-ThetaMenos[i,r])/ThetaMenos[i,r])/4.)
+sigma=sigma.tocsr()
+nu=nu.tocsr()
 
 
 
