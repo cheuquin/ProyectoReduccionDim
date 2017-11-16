bb21a69d58769bf67b8047f30f0eeb9748fa692d
dortiz5 <dortiz5@uc.cl>
2017-11-16 00:49:22 -0300

    gthxghgshf

--- mask.m
+++ mask.m
@@ -1,18 +1,22 @@
-n = 50;
+n = 400;
 d = 50;
 e = 0.1;
 
-s = ceil(log(n)/e);
-m = ;
+
+resto = ceil((1/e) - ceil(log(n)/e^2));
+
+m = ceil(log(n)/e^2) + resto;
+s = ceil(m*e);
 t = 3/(2*log(2)*m);
 
 p = ones(m,d)*e;
 Bq = reshape(1:m,1/e,s);
 
-p(Bq(:,1),1)=[1 zeros(length(Bq(:,1))-1,1)']; %%Arreglar, primera iteracion
+p(Bq(:,1),1)=zeros(length(Bq(:,1)),1); %%Arreglar, primera iteracion
+p(Bq(1,1),1)=1;
 
 for q = 1:s % bloque q
-    for l = 2:d % col l -> bloque (q,l) UNIFORME
+    for l = 2:d % col l -> bloque (q,l) UNIFORME MIRAR INICIAL
         
         %% Sumatoria alfas
         alfa = zeros(length(Bq(:,1)),1);
@@ -22,8 +26,15 @@ for q = 1:s % bloque q
                 
                 %% productoria D
                 D = 1;
-                for q_bar = 1:q-1
-                    rho = sum(p(Bq(:,q_bar),j).*p(Bq(:,q_bar),k));
+                
+                if q==1 % primeras filas de bloques
+                    sust=0;
+                else
+                    sust=1;
+                end
+                
+                for q_bar = 1:q-sust
+                    rho = sum(p(Bq(:,q_bar),j).*p(Bq(:,q_bar),l));
                     D = D*(1+rho);
                     
                 end
